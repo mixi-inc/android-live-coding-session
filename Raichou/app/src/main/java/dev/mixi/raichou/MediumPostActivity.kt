014ab708b2f1649f699f4d83c3929d664fc55585
hkurokawa <hirosh.kurokawa@gmail.com>
2019-03-26 19:58:54 +0900

    Add permission for READ_EXTERNAL_STORAGE.

--- Raichou/app/src/main/java/dev/mixi/raichou/MediumPostActivity.kt
+++ Raichou/app/src/main/java/dev/mixi/raichou/MediumPostActivity.kt
@@ -1,28 +1,74 @@
 package dev.mixi.raichou
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dev.mixi.raichou.databinding.ActivityMediumPostBinding
 import dev.mixi.raichou.databinding.ItemMediumBinding
 
+private const val REQ_CODE_STORAGE_PERMISSION = 1
+
 class MediumPostActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMediumPostBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_medium_post)
-        val binding = DataBindingUtil.setContentView<ActivityMediumPostBinding>(this, R.layout.activity_medium_post)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_medium_post)
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                REQ_CODE_STORAGE_PERMISSION
+            )
+            return
+        }
+
+        showList()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQ_CODE_STORAGE_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    showList()
+                } else {
+                    // TODO: show dialog
+                }
+            }
+        }
+    }
+
+    private fun showList() {
         binding.list.adapter = MyAdapter(emptyList())
         binding.list.layoutManager = GridLayoutManager(this, 3)
         binding.list.setHasFixedSize(true)
+
+        val cursor = contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            arrayOf(MediaStore.Images.ImageColumns.DATA),
+            null,
+            null,
+            null
+        )
     }
 
     companion object {
