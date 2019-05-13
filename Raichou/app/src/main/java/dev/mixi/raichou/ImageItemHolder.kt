12f07546ab32ba6be80b2be426612a558f93db4e
wasabeef <dadadada.chop@gmail.com>
2019-05-13 18:07:54 +0900

    Convert to ListAdapter

---
+++ Raichou/app/src/main/java/dev/mixi/raichou/ImageItemHolder.kt
@@ -0,0 +1,6 @@
+package dev.mixi.raichou
+
+import androidx.recyclerview.widget.RecyclerView
+import dev.mixi.raichou.databinding.ItemMediumBinding
+
+class ImageItemHolder(val binding: ItemMediumBinding) : RecyclerView.ViewHolder(binding.root)
