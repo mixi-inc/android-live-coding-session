de56e0e5601100c3aee28dd8ad678c4fde776941
punchdrunker <nanao.hhk@gmail.com>
2019-05-16 10:38:32 +0900

    Revert "Merge branch 'master' into refactor"
    This reverts commit ce7cb2d1beed6860ad7d69727b9e05982eb5f8cf, reversing
    changes made to c0a4def3d60e511c53f83efbcc4e9248e5f6636a.

--- Raichou/app/src/main/java/dev/mixi/raichou/ImageItemHolder.kt
+++
@@ -1,6 +0,0 @@
-package dev.mixi.raichou
-
-import androidx.recyclerview.widget.RecyclerView
-import dev.mixi.raichou.databinding.ItemMediumBinding
-
-class ImageItemHolder(val binding: ItemMediumBinding) : RecyclerView.ViewHolder(binding.root)
--- Raichou/app/src/main/java/dev/mixi/raichou/MainActivity.kt
+++ Raichou/app/src/main/java/dev/mixi/raichou/MainActivity.kt
@@ -2,13 +2,12 @@ package dev.mixi.raichou
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.net.toUri
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import dev.mixi.raichou.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
+class MainActivity : AppCompatActivity() {
 
     private val navController: NavController by lazy {
         Navigation.findNavController(this, R.id.nav_fragment_main)
