320382d1880bbf5c5df4afa3170b34f31567ab81
hkurokawa <hirosh.kurokawa@gmail.com>
2019-06-18 20:12:43 +0900

    Introduce nav_main navigation.

--- Raichou/app/src/main/java/dev/mixi/raichou/MainActivity.kt
+++ Raichou/app/src/main/java/dev/mixi/raichou/MainActivity.kt
@@ -2,15 +2,10 @@ package dev.mixi.raichou
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.net.toUri
 import androidx.databinding.DataBindingUtil
-import com.google.firebase.firestore.FirebaseFirestore
 import dev.mixi.raichou.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
 
 class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
 
@@ -18,18 +13,5 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
         super.onCreate(savedInstanceState)
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        binding.btnPost.setOnClickListener { startActivity(MediumPostActivity.createIntent(this)) }
-
-        launch(Dispatchers.Main) {
-            val db = FirebaseFirestore.getInstance()
-            val snapshot = db.collection("images").get().await()
-
-            val list = snapshot.map {
-                ImageItemModel(it.data["url"].toString().toUri())
-            }
-            binding.list.adapter = ImageListAdapter(selectable = false).apply {
-                submitList(list)
-            }
-        }
     }
 }
