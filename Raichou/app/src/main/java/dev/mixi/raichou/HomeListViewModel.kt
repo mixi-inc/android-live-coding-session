2f764c3d6aab2fad6282c6ac35cf2ca440866771
takahirom <takam.dev@gmail.com>
2019-07-30 09:06:04 +0900

    Detect change in image lis using Kotlin flow

--- Raichou/app/src/main/java/dev/mixi/raichou/HomeListViewModel.kt
+++ Raichou/app/src/main/java/dev/mixi/raichou/HomeListViewModel.kt
@@ -4,17 +4,35 @@ import androidx.core.net.toUri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.collect
 
 class HomeListViewModel : ViewModel() {
 
     val images = liveData {
-        val db = FirebaseFirestore.getInstance()
-        val snapshot = db.collection("images").get().await()
-
-        val list = snapshot.map {
-            ImageItemModel(it.data["url"].toString().toUri())
+        // https://medium.com/@elizarov/callbacks-and-kotlin-flows-2b53aa2525cf
+        callbackFlow {
+            val listenerRegistration = FirebaseFirestore
+                .getInstance()
+                .collection("images")
+                .addSnapshotListener { snapshot, e ->
+                    when {
+                        e != null ->
+                            close(e)
+                        snapshot == null ->
+                            close()
+                        else -> {
+                            val list = snapshot.map {
+                                ImageItemModel(it.data["url"].toString().toUri())
+                            }
+                            offer(list)
+                        }
+                    }
+                }
+            awaitClose { listenerRegistration.remove() }
+        }.collect { list ->
+            emit(list)
         }
-        emit(list)
     }
 }
