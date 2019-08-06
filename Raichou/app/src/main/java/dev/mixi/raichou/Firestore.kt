f67cbf00a938d1a75290480fce292f60c6e1c471
takahirom <takam.dev@gmail.com>
2019-08-06 20:19:36 +0900

    Make HomeListViewModel.images reactive

---
+++ Raichou/app/src/main/java/dev/mixi/raichou/Firestore.kt
@@ -0,0 +1,32 @@
+package dev.mixi.raichou
+
+import androidx.core.net.toUri
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.map
+
+fun FirebaseFirestore.images(): Flow<List<ImageItemModel>> {
+    return collection("images")
+        .toFlow()
+        .map { snapshot ->
+            snapshot.map {
+                ImageItemModel(it.data["url"].toString().toUri())
+            }
+        }
+}
+
+private fun CollectionReference.toFlow(): Flow<QuerySnapshot> {
+    return callbackFlow<QuerySnapshot> {
+        val listenerRegistration = addSnapshotListener { snapshot, exception ->
+            if (exception != null) close(exception)
+            else if (snapshot != null) {
+                offer(snapshot)
+            }
+        }
+        awaitClose { listenerRegistration.remove() }
+    }
+}
--- Raichou/app/src/main/java/dev/mixi/raichou/HomeListViewModel.kt
+++ Raichou/app/src/main/java/dev/mixi/raichou/HomeListViewModel.kt
@@ -1,20 +1,23 @@
 package dev.mixi.raichou
 
-import androidx.core.net.toUri
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 
 class HomeListViewModel : ViewModel() {
+    val images = FirebaseFirestore.getInstance()
+        .images()
+        .toLiveData()
+}
 
-    val images = liveData {
-        val db = FirebaseFirestore.getInstance()
-        val snapshot = db.collection("images").get().await()
-
-        val list = snapshot.map {
-            ImageItemModel(it.data["url"].toString().toUri())
-        }
-        emit(list)
+private fun <T> Flow<T>.toLiveData(): LiveData<T> {
+    return liveData {
+        this@toLiveData
+            .collect {
+                emit(it)
+            }
     }
 }
